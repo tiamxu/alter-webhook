d9052207d2725b7371509098210c7368152db460
Your Name <you@example.com>
2023-12-10 22:52:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# alter-webhook
+prometheus -> altermanager -> alter-webhook -> webhook
+
+# 测试
+```
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{     "receiver": "web.hook",     "status": "firing",     "alerts": [{         "status": "firing",         "labels": {             "alertname": "node",             "instance": "192.168.0.10",             "job": "test1111",             "severity": "critical"         },         "annotations": {             "description": "test1 192.168.0.10 节点断联已超过1分钟！",             "summary": "192.168.0.10 down "         },         "startsAt": "2022-04-28T08:44:23.05Z",         "endsAt": "0001-01-01T00:00:00Z",         "generatorURL": "http://localhost.localdomain:19090/graph?g0.expr=up+%3D%3D+0\u0026g0.tab=1",         "fingerprint": "726681bf4674e8a5"     }, {         "status": "firing",         "labels": {             "alertname": "node",             "instance": "192.168.1.10",             "job": "test2222",             "severity": "critical"         },         "annotations": {             "description": "test2 192.168.1.10 节点断联已超过1分钟！",             "summary": "192.168.1.10 down "         },         "startsAt": "2022-04-28T08:44:23.05Z",         "endsAt": "0001-01-01T00:00:00Z",         "generatorURL": "http://localhost.localdomain:19090/graph?g0.expr=up+%3D%3D+0\u0026g0.tab=1",         "fingerprint": "726681bf4674e8a5"     } ],     "groupLabels": {         "alertname": "node"     },     "commonLabels": {         "alertname": "node",         "instance": "192.168.0.10",         "job": "rh7",         "severity": "critical"     },     "commonAnnotations": {         "description": "rh7 192.168.0.10 节点断联已超过1分钟！",         "summary": "192.168.0.10 down "     },     "externalURL": "http://192.168.0.10:19092",     "version": "4",     "groupKey": "{}:{alertname=\"node\"}",     "truncatedAlerts": 0 }' \
+ http://localhost:8080/v1/webhook
+```
---
+++ main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+// func main() {
+// 	router := gin.Default()
+// 	router.POST("/webhook", func(c *gin.Context) {
+// 		var notification Notification
+// 		err := c.ShouldBind(&notification)
+// 		if err != nil {
+// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 			return
+// 		}
+// 		c.JSON(http.StatusOK, gin.H{"message": " successful receive alert notification message!"})
+// 		fmt.Printf("alerts:%#v\n", notification.Alerts)
+// 		if notification.Status == "firing" {
+// 			for _, alert := range notification.Alerts {
+// 				fmt.Printf("alert:%v\n", alert)
+// 				if alert.Status == "firing" {
+// 					go HandleWebhook(alert)
+// 				}
+// 			}
+// 		}
+// 	})
+// 	router.Run(":8080")
+// }
+
+// func HandleWebhook(alert Alert) {
+// 	url := "http://localhost:9000/hooks/webhook"
+// 	contentType := "application/json"
+// 	data, err := json.Marshal(&alert)
+// 	if err != nil {
+// 		fmt.Println("json marshal error")
+// 		return
+// 	}
+// 	resp, err := http.Post(url, contentType, strings.NewReader(string(data)))
+// 	if err != nil {
+// 		log.Fatal(err)
+// 		return
+// 	}
+// 	defer resp.Body.Close()
+// 	b, err := io.ReadAll(resp.Body)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 		return
+// 	}
+// 	fmt.Println(string(b))
+// }
+
+func AlertMarshal(alert *Alert) string {
+	data, err := json.Marshal(&alert)
+	if err != nil {
+		fmt.Printf("AlertMarshal json marshal error ,%v", err)
+		return ""
+	}
+	return string(data)
+}
+
+func RequestWebHook(data string) {
+	url := "http://localhost:9000/hooks/webhook"
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(data)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json;charset=utf-8")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Printf("webhook响应数据: %v\n", string(body))
+}
+
+func HandlerWebhook(c *gin.Context) {
+	var notification Notification
+	if err := c.ShouldBind(&notification); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	if notification.Status == "firing" {
+		for _, alert := range notification.Alerts {
+			fmt.Printf("alert:%v\n", alert)
+			if alert.Status == "firing" {
+				RequestWebHook(AlertMarshal(&alert))
+			}
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"msg": "success",
+	})
+}
+
+func main() {
+	// 设置gin运行模式，可选：DebugMode、ReleaseMode、TestMode
+
+	gin.SetMode(gin.ReleaseMode)
+	// 关闭控制台日志颜色
+	gin.DisableConsoleColor()
+	// 记录到文件
+	f, _ := os.OpenFile("gin.log", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	gin.DefaultWriter = io.MultiWriter(f)
+
+	r := gin.Default()
+
+	v1 := r.Group("/v1")
+	{
+		v1.POST("/webhook", HandlerWebhook)
+	}
+
+	// 设置程序优雅退出
+	srv := &http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+	log.Println("Server exiting")
+}
